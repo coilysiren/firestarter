35e70bbc53cc9213009ac77bc1554b9598d65bb3
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-08-19 18:19:20 -0700

    allowed more flexible paths

--- scripts/cms.py
+++ scripts/cms.py
@@ -5,7 +5,7 @@ def build_html (path):
     '''
     # try for markdown
     try:
-        with open('paths/'+path+'.md', 'r') as md_data:
+        with open(path+'.md', 'r') as md_data:
             text = md_data.read()
         # create html
         import markdown
@@ -14,7 +14,7 @@ def build_html (path):
     except IOError: pass
     # try for html
     try:
-        with open('paths/'+path+'.html', 'r') as html_data:
+        with open(path+'.html', 'r') as html_data:
             text = html_data.read()
         return text
     # if neither? fail
